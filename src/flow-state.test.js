c9ba91ff85fad6dae26611f2ddb01598ea2aa93f
Kahlil Lechelt <hello@kahlil.info>
2019-07-25 08:56:00 +0200

    clean up

--- src/flow-state.test.js
+++ src/flow-state.test.js
@@ -1,6 +1,6 @@
-import { createFlowState } from "../src/flow-state.js";
-import { skip, filter, map } from "rxjs/operators";
-import test from "ava";
+import { createFlowState } from './flow-state.js';
+import { skip, filter, map } from 'rxjs/operators';
+import test from 'ava';
 
 let flowState;
 
@@ -12,69 +12,69 @@ test.afterEach.always(t => {
   flowState = null;
 });
 
-test("getAction$()", t => {
+test('getAction$()', t => {
   t.plan(2);
   const dispatcher$ = flowState.getAction$();
   t.is(
     typeof dispatcher$.next,
-    "function",
-    "The dispatcher stream has a next function."
+    'function',
+    'The dispatcher stream has a next function.'
   );
   t.is(
     typeof dispatcher$.subscribe,
-    "function",
-    "The dispatcher stream has a subscribe function."
+    'function',
+    'The dispatcher stream has a subscribe function.'
   );
 });
 
-test("dispatch()", t => {
+test('dispatch()', t => {
   t.plan(1);
   const action$ = flowState.getAction$();
   const return$ = action$.pipe(skip(1)).subscribe(action => {
     t.deepEqual(
       action,
-      { type: "TEST_ACTION", payload: 1 },
-      "Correct action is dispatched."
+      { type: 'TEST_ACTION', payload: 1 },
+      'Correct action is dispatched.'
     );
   });
   // Set the actioncreators.
-  flowState.dispatch({ type: "TEST_ACTION", payload: 1 });
+  flowState.dispatch({ type: 'TEST_ACTION', payload: 1 });
   return return$;
 });
 
-test("createState$()", t => {
+test('createState$()', t => {
   t.plan(1);
-  flowState.dispatch({ type: "TEST_ACTION", payload: 1 });
+  flowState.dispatch({ type: 'TEST_ACTION', payload: 1 });
   const return$ = flowState
     .createState$(
       {
         testAction: (action, state) => {
           state.push(action.payload);
           return state;
-        }
+        },
       },
       []
     )
     .subscribe(state => {
-      t.deepEqual(state, [1], "Correct state is created.");
+      t.deepEqual(state, [1], 'Correct state is created.');
     });
   return return$;
 });
 
-test("runSideEffects()", t => {
+test('runSideEffects()', t => {
   t.plan(1);
   flowState = createFlowState();
   const action$ = flowState.getAction$();
   const sideEffect = action$ =>
     action$.pipe(
-      filter(({ type }) => type === "INIT"),
-      map(() => ({ type: "FX_ACTION" }))
+      filter(({ type }) => type === 'INIT'),
+      map(() => ({ type: 'FX_ACTION' }))
     );
   flowState.runSideEffects(sideEffect);
   const return$ = action$
-    .pipe(filter(({ type }) => type === "FX_ACTION"))
+    .pipe(filter(({ type }) => type === 'FX_ACTION'))
     .subscribe(({ type }) =>
-      t.is(type, "FX_ACTION", "Correct state is created.")
+      t.is(type, 'FX_ACTION', 'Correct state is created.')
     );
   return return$;
 });
